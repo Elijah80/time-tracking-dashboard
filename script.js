8fd54dbd2469760b729dfc39bff9d10ffbc2a312
Elijah Cravens <elijahcravens46@gmail.com>
2023-10-13 20:00:35 -0600

    Created Profile Section
    Created profile section for the sidebar and styled to match close to
    the design image

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-const container = document.querySelector('.container');
+const container = document.querySelector('.container')
+
+fetch('./data.json')
+	.then(response => response.json())
+	.then(data => {
+    const name = data[0].firstName + ' ' + data[0].lastName;
+    const timeFrames = data
+    .filter(item => item.timeframes)
+    .map(item => Object.keys(item.timeframes))
+    .slice(0, 1);
+
+    createSidebar(name, timeFrames);
+	})
+
+function createElement(type, className) {
+	const element = document.createElement(type)
+
+	element.classList.add(className);
+
+  return element;
+}
+
+function createSidebar(name, timeframes){
+  const sideBar = createElement('div', 'sidebar');
+  const profileSection = createElement('div', 'profile-section');
+  const profileImage = createElement('img', 'profile-image');
+  const header = createElement('p', 'header');
+  const userName = createElement('p', 'name');
+  const linkSection = createElement('div', 'link-section');
+
+  profileImage.setAttribute('src', './images/self.png');
+
+  header.textContent = 'Report for'
+  userName.textContent = name;
+
+  profileSection.appendChild(profileImage);
+  profileSection.appendChild(header);
+  profileSection.appendChild(userName);
+
+  console.log(timeframes);
+
+  sideBar.appendChild(profileSection);
+  container.appendChild(sideBar);
+}
